cdde1c65de4347ea4b3e05fdfa46eb198ff4c409
coder123 <40211183+marvinChomba@users.noreply.github.com>
2022-10-31 13:49:56 +0200

    Update server.ino

--- server.ino
+++ server.ino
@@ -1,4 +1,5 @@
 #include <SPI.h>
+  #include <TimeLib.h>
 #include <RH_RF95.h>
  
 // Singleton instance of the radio driver
@@ -10,13 +11,16 @@ void setup()
  
 //  pinMode(led, OUTPUT);     
   Serial.begin(9600);
+  setTime(13,45,0,31,10,22);
   while (!Serial) ; // Wait for serial port to be available
   if (!rf95.init()) {
         Serial.println("init failed from server");  
 
+  } else {
+    Serial.println("Connected");
   }
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
- rf95.setSpreadingFactor(7);
+ rf95.setSpreadingFactor(12);
  rf95.setCodingRate4(5);
  rf95.setTxPower(20, false);
  rf95.setSignalBandwidth(125000);
@@ -31,19 +35,14 @@ void loop()
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len))
     {
-//      digitalWrite(led, HIGH);
 //      RH_RF95::printBuffer("request: ", buf, len);
 //      Serial.print("got request: ");
-//      Serial.println((char*)buf);
-//      Serial.print("RSSI: ");
+      Serial.println((char*)buf);
+      String time_now = String(hour()) + ":" + String(minute()) + ":" + String(second());
+      Serial.println(time_now);
+      Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
       
-      // Send a reply
-      uint8_t data[] = "And hello back to you";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-//      Serial.println("Sent a reply");
-//       digitalWrite(led, LOW);
     }
     else
     {
